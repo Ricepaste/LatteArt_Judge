395657c6e4c8935bda24c78bc212457183db23c7
Ricepaste <n22126@gmail.com>
2023-11-01 20:48:00 +0800

    可以一次裁切多張照片

--- main/LatteArtCNN.py
+++ main/LatteArtCNN.py
@@ -1,6 +1,7 @@
 import torch
 import cv2
 
+show_img = False
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Current Running Device: ", end="")
@@ -12,20 +13,28 @@ model = torch.hub.load('.\\yolov5', 'custom', source='local',
                        path='.\\Crop_Model\\exp62_v3\\weights\\best.pt',
                        force_reload=True).to(device)
 
-imgs = ['.\\main\\inputPhoto\\1.jpg']
+imgs = ['.\\main\\inputPhoto\\{}.jpg'.format(i)for i in range(2)]
 results = model(imgs, size=64)
 results.print()
 
-results.show()
-print(results.xyxy)
+if (show_img):
+    results.show()
+# print(results.xyxy)
 
-for img in imgs:
-    for i in range(results.xyxy[0].cpu().numpy().shape[0]):
-        crop_img = cv2.imread(img)
-        x_lt = results.xyxy[0].cpu().numpy()[i][0]
-        y_lt = results.xyxy[0].cpu().numpy()[i][1]
-        x_rb = results.xyxy[0].cpu().numpy()[i][2]
-        y_rb = results.xyxy[0].cpu().numpy()[i][3]
+output_index = 0
+for img in range(len(imgs)):
+    for i in range(results.xyxy[img].cpu().numpy().shape[0]):
+        crop_img = cv2.imread(imgs[img])
+        x_lt = results.xyxy[img].cpu().numpy()[i][0]
+        y_lt = results.xyxy[img].cpu().numpy()[i][1]
+        x_rb = results.xyxy[img].cpu().numpy()[i][2]
+        y_rb = results.xyxy[img].cpu().numpy()[i][3]
         crop_img = crop_img[int(y_lt):int(y_rb), int(x_lt):int(x_rb)]
-        cv2.imshow("cropped", crop_img)
-        cv2.waitKey(0)
+
+        if (show_img):
+            cv2.imshow("cropped", crop_img)
+            cv2.waitKey(0)
+
+        output_index += 1
+        path = '.\\main\\cropPhoto\\cropPhotocrop_{}.jpg'.format(output_index)
+        cv2.imwrite(path, crop_img)
