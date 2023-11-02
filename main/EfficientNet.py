b3aa31bbca2ee470a68c914446fc89ff4238574a
Ricepaste <n22126@gmail.com>
2023-11-02 16:46:05 +0800

    小黃瓜

--- main/EfficientNet.py
+++ main/EfficientNet.py
@@ -1,13 +1,13 @@
 from torchvision.io import read_image
 import torchvision.models as models
-from torchvision.models import EfficientNet_B0_Weights
+from torchvision.models import EfficientNet_B1_Weights
 # import torch
 
-img = read_image(".\\main\\cropPhoto\\cropPhotocrop_4.jpg")
+img = read_image(".\\main\\cropPhoto\\cropPhotocrop_5.jpg")
 
 # Step 1: Initialize model with the best available weights
-weights = EfficientNet_B0_Weights.DEFAULT
-model = models.efficientnet_b0(weights=weights)
+weights = EfficientNet_B1_Weights.DEFAULT
+model = models.efficientnet_b1(weights=weights)
 model.eval()
 
 # Step 2: Initialize the inference transforms
