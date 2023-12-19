ac1624577c533d0351a54bf5b41f7a6292962234
BangDonKiller <parisdata@gmail.com>
2023-12-19 16:53:26 +0800

    可存取隊伍連結

--- NCAA/InfoGet.py
+++ NCAA/InfoGet.py
@@ -16,16 +16,46 @@ for i in range(len(Division)):
 
 index = 0
 for i in range(len(Division)):
+    # 創建資料夾
     if (i == 3 or i == 6 or i == 9 or i == 10):
         plus = 2
     else:
         plus = 1
+        
     team_folder = os.path.join(folder, Division[i])
+    
+    team_name = []
+    team_team_link = []
     for j in range(index, index + plus):
         td_tags = soup.find_all('tbody')[j].find_all('tr')[1:]
         
         for td in td_tags:
-            if (td.find('a').text not in os.listdir(team_folder)):
-                os.mkdir(os.path.join(team_folder, td.find('a').text))
+            team_name.append(td.find('a').text)
+            try:
+                if (td.find('a').text not in os.listdir(team_folder)):
+                    os.mkdir(os.path.join(team_folder, td.find('a').text))
+            except:
+                continue
+        # print(len(team_name))
+        # 抓取隊伍連結
+
+        for tr in soup.find_all('tbody')[j].find_all('tr'):
+            link = tr.find('a')
+            if link:
+                team_team_link.append(link['href'])
+        # print(len(team_team_link))
     index += plus
+    # ---------------------------------------------        
+            
+
+    # for j in range(len(team_team_link)):
+    #     team_web = requests.get("http://covers.com"+team_team_link[j])
+    #     team_soup = bs(team_web.text, 'html.parser')
+        
+    
+    
+
+
+
+
     
