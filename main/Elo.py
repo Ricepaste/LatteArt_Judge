c2ba19dc7dbc5fb438da1e5de62a513a51e08b1a
Ricepaste <n22126@gmail.com>
2023-12-30 16:18:16 +0800

    可從資料中擷取出贏家校名

--- main/Elo.py
+++ main/Elo.py
@@ -5,38 +5,44 @@ import sys
 
 
 def data_load(year):
+    FMT = 3
+    if year >= 2013:
+        FMT = 4
     # PATH
     path = f"./spider/rank_data/{year}-{year+1}_Record.csv"
     print(path)
-    df = pd.read_csv(path, sep='\t')
-    return np.array(df)
+    df = np.array(pd.read_csv(path, sep='\t'))
+
+    winner = df[:, FMT]
+    for i in range(winner.size):
+        while '\xa0' in winner[i]:
+            # print(winner[i])
+            winner[i] = winner[i][1:]
+        if '(' in winner[i]:
+            # print(winner[i])
+            winner[i] = winner[i][:winner[i].index('(')]\
+                + winner[i][winner[i].index(')')+1:]
+            # print(winner[i])
+        if ' ' in winner[i]:
+            # print(winner[i])
+            winner[i] = winner[i][:winner[i].index(' ')]\
+                + winner[i][winner[i].index(' ')+1:]
+            # print(winner[i])
+
+    return np.array(winner)
 
 
 def main():
+    for i in range(2003, 2023):
+        x = data_load(i)
+        print(x)
     pass
 
 
 def debug():
-    x = data_load(2003)
-    winner = x[:, 3]
-    for i in range(winner.size):
-        while '\xa0' in winner[i]:
-            print(winner[i])
-            winner[i] = winner[i][1:]
-        if '(' in winner[i]:
-            print(winner[i])
-            winner[i] = winner[i][:winner[i].index('(')]\
-                + winner[i][winner[i].index(')')+1:]
-            print(winner[i])
-        if ' ' in winner[i]:
-            print(winner[i])
-            winner[i] = winner[i][:winner[i].index(' ')]\
-                + winner[i][winner[i].index(' ')+1:]
-            print(winner[i])
-    # print(winner.size)
-    # print(len(set(winner)))
+    pass
 
 
 if __name__ == '__main__':
-    # main()
-    debug()
+    main()
+    # debug()
