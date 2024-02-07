c7851da41e66375d604657efb32f55134f418784
Ricepaste <n22126@gmail.com>
2024-02-07 23:06:43 +0800

    調整超參數

--- main/Kendall.py
+++ main/Kendall.py
@@ -13,14 +13,15 @@ Hawai'i = Hawaii
 
 # build a dictionary to store the team name
 team_name = {
-    'TCU' : 'TexasChristian',
-    'USC' : 'SouthernCalifornia',
-    'LSU' : 'LouisianaState',
-    'BYU' : 'BrighamYoung',
-    'UCF' : 'CentralFlorida',
-    'NCState' : 'NorthCarolina State',
-    'SanJoseState' : 'SanJose State',
-    "Hawai'i" : "Hawaii",
+    'TCU': 'TexasChristian',
+    'USC': 'SouthernCalifornia',
+    'LSU': 'LouisianaState',
+    'BYU': 'BrighamYoung',
+    'UCF': 'CentralFlorida',
+    'NCState': 'NorthCarolina State',
+    'SanJoseState': 'SanJose State',
+    "Hawai'i": "Hawaii",
+    "OleMiss": "Mississippi",
 }
 
 
@@ -49,17 +50,19 @@ CORR = []
 for year in range(2003, 2023):
     print(f"{year}-{year+1} 年度")
     try:
-        elo_rank = pd.read_csv(f'./spider/rank_data/{year}-{year+1}_elo100_K32_shuffleFalse_stepLRFalse_inheritTrue.csv', sep = '\t')
-        ap_rank = pd.read_csv(f'./spider/rank_data/{year}-{year+1}_ap-poll.csv', sep = '\t')
+        elo_rank = pd.read_csv(
+            f'./spider/rank_data/{year}-{year+1}_elo100_K32_shuffleTrue_stepLRTrue_inheritTrue.csv', sep='\t')
+        ap_rank = pd.read_csv(
+            f'./spider/rank_data/{year}-{year+1}_ap-poll.csv', sep='\t')
         # get the header of the dataframe
         elo_header = elo_rank.columns.values.tolist()[0]
         elo_rank = elo_rank.values.tolist()
-        
+
         ap_header = ap_rank.columns.values.tolist()[1]
         ap_rank = ap_rank.values.tolist()
         # print(header)
         # print(elo_rank)
-        
+
         elo_array = [elo_header]
         ap_array = [ap_header]
         for i in range(23):
@@ -71,15 +74,15 @@ for year in range(2003, 2023):
             ap_array.append(ap_rank[i][1])
         print("已讀取檔案")
     except:
-        print("沒讀到檔案")    
+        print("沒讀到檔案")
 
     print("Elo前24名次隊伍 : ", elo_array)
     print("Ap前24名次隊伍 : ", ap_array)
     print("Kendall tau係數 : ", Kendall_tau(elo_array, ap_array))
-    
+
     CORR.append(Kendall_tau(elo_array, ap_array))
-    
-    
+
+
 # print("2003~2023 Kendall's tau 係數 : ", CORR)
 
 for i in range(len(CORR)):
