a5c8fab3812de1289384992d97015863eb56fcb9
Ricepaste <n22126@gmail.com>
2024-03-10 21:05:37 +0800

    測試是否正確

--- main/random_walk_matrix.py
+++ main/random_walk_matrix.py
@@ -15,13 +15,12 @@ def deal_team_name(team_name):
     return team_name
 
 
-
 # 例外狀況 有比賽被取消，比分先暫定0:0 (手動加上比分)
 # 3	Sep 14, 2013	2:00 PM	Sat	Fresno State		@	Colorado		Game Cancelled
 # 1	Sep 5, 2015	7:30 PM	Sat	McNeese State		@	(14) Louisiana State		Cancelled due to weather
 
-
-for year in range(2003, 2004):
+# for year in range(2003, 2004):
+for year in range(2023, 2024):
 
     record = pd.read_csv(
         f'./spider/rank_data/{year}-{year+1}_Record.csv', sep="\t", header=None)
@@ -52,11 +51,11 @@ for year in range(2003, 2004):
     # use list to store the team name and delete the duplicate team name
     all_team_name = list(set(win_team_name + lose_team_name))
     # print(all_team_name)
-    
+
     # build a 2D list to store the lose-win matrix
     matrix = [[0.001 for i in range(len(all_team_name))]
               for j in range(len(all_team_name))]
-    
+
     # 計算單支隊伍敗場數
     lose_count = {}
     for i in range(len(lose_team_name)):
@@ -64,18 +63,19 @@ for year in range(2003, 2004):
             lose_count[lose_team_name[i]] = 1
         else:
             lose_count[lose_team_name[i]] += 1
-            
+
     # 對於輸過的隊伍，計算走到該隊伍的機率
     for i in range(len(win_team_name)):
         win_index = all_team_name.index(win_team_name[i])
         lose_index = all_team_name.index(lose_team_name[i])
-        probability = (1-0.001*(len(all_team_name)-lose_count[lose_team_name[i]]))/lose_count[lose_team_name[i]]
-        
+        probability = (1-0.001*(len(all_team_name) -
+                       lose_count[lose_team_name[i]]))/lose_count[lose_team_name[i]]
+
         if (matrix[lose_index][win_index] != 0.001):
             matrix[lose_index][win_index] += probability
         else:
             matrix[lose_index][win_index] = probability
-    
+
     # 檢查矩陣列和是否接近1
     for i in range(len(all_team_name)):
         sum = 0
