fb01ddf33c28424849ee76ff1bf9e2f2b0d2c401
Ricepaste <n22126@gmail.com>
2024-04-19 22:33:08 +0800

    thurstone 完成

--- Mplus/MET_Maydeu-Olivares_web/test.py
+++ Mplus/MET_Maydeu-Olivares_web/test.py
@@ -1,31 +1,40 @@
-import semopy
-
-model = '''
-f1 BY pc12 pc13 pc14 pc15 pc16 pc17@1;
-f2 BY pc23 pc24 pc25 pc26 pc27@1;
-f3 BY pc34 pc35 pc36 pc37@1;
-f4 BY pc45 pc46 pc47@1;
-f5 BY pc56 pc57@1;
-f6 BY pc67@1;
-
-pc12-pc67*.1;
-
-f1@1;
-f7@1;
-
-f2 WITH f1;
-f3 WITH f1 f2;
-f4 WITH f1 f3;
-f5 WITH f1 f4;
-f6 WITH f1 f5;
-
-f7 WITH f1 f6@0;
-
-pc12-pc67*.1;
-
-OUTPUT: TECH1; TECH5;
+trans = '''
+    F1                 0.155
+    F2                 0.147
+    F3                 0.146
+    F4                 0.147
+    F5                 0.147
+    F6                 0.148
+    F7                 0.146
+    F8                 0.146
+    F9                 0.153
+    F10                0.147
+    F11                0.147
+    F12                0.147
+    F13                0.147
+    F14                0.147
+    F15                0.146
+    F16                0.147
+    F17                0.147
+    F18                0.146
+    F19                0.000
 '''
+team = ['NotreDame', 'Michigan', 'PennState', 'AppalachianState', 'Clemson', 'TexasA&M', 'Kentucky', 'Florida', 'Alabama', 'Oklahoma',
+        'Army', 'Syracuse', 'OhioState', 'LouisianaState', 'Georgia', 'CentralFlorida', 'Cincinnati', 'Washington', 'WashingtonState']
 
-model_fit = semopy.Model(model)
-model_fit.fit('cars pc.dat')
-model_fit.inspect()
+for i in range(len(team)):
+    trans = trans.replace(f'    F{i+1} ', team[i])
+
+rank = {}
+for line in trans.split('\n'):
+    if line == '':
+        continue
+    line = line.split()
+    rank[line[0]] = float(line[1])
+
+num = 1
+for key, value in sorted(rank.items(), key=lambda x: x[1], reverse=True):
+    print(f'{num}\t{key:25s}{value}')
+    num += 1
+
+# print(trans)
