732cd8795f353e08418714b7224289ac4e099141
Ricepaste <n22126@gmail.com>
2024-06-06 17:20:53 +0800

    feat: Add Siamese_Model for feature extraction and classification
    This commit adds a new file, Siamese_Model.py, which contains the implementation of a Siamese neural network (SNN) for feature extraction and classification. The SNN takes two input images and extracts features using a pretrained model. It then concatenates the features and passes them through a classifier to predict a binary output. The last layer of the classifier has been modified to fit the specific problem.
    
    The SNN is implemented using PyTorch and includes various layers such as linear, ReLU, sigmoid, and dropout. The changes also include updating the last layer of the efficientNet_b0 model to fit the problem, as commented out in the code.
    
    This feature will improve the accuracy of the classification model and enable better analysis of the input images.

---
+++ main/Siamese_Model.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+class SNN(nn.Module):
+
+    def __init__(self, pretrained_model):
+        super(SNN, self).__init__()
+        features_and_avgpool = nn.Sequential(
+            pretrained_model.features, pretrained_model.avgpool
+        )
+        self.feature_extractor = features_and_avgpool
+        self.classfier = nn.Sequential(
+            nn.Linear(2 * 1280 * 1 * 1, 500),
+            nn.ReLU(inplace=True),
+            nn.Linear(500, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x1, x2):
+        x1 = self.feature_extractor(x1)
+        x2 = self.feature_extractor(x2)
+        x1 = x1.view(-1, 1280 * 1 * 1)
+        x2 = x2.view(-1, 1280 * 1 * 1)
+        x = torch.cat((x1, x2), dim=1)
+        x = self.classfier(x)
+        return x
+
+
+# torch.nn.FeatureAlphaDropout(p=0.2, inplace=False),
+
+"""
+# change the last layer of the b3 model to fit our problem
+model._modules['classifier'] = torch.nn.Sequential(
+    # torch.nn.FeatureAlphaDropout(p=0.2, inplace=False),
+    torch.nn.Linear(1280, 20000),
+    torch.nn.Sigmoid(),
+    torch.nn.Dropout(p=0.2, inplace=False),
+    torch.nn.Linear(20000, 5000),
+    torch.nn.LeakyReLU(),
+    torch.nn.Dropout(p=0.2, inplace=False),
+    torch.nn.Linear(5000, 100),
+    torch.nn.LeakyReLU(),
+    torch.nn.Dropout(p=0.2, inplace=False),
+    torch.nn.Linear(100, 1),
+    # torch.nn.Linear(1280, 1),
+)
+"""
