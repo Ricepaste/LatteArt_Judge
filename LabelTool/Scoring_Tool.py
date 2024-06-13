beaad45a3b6b277c490395f9d9f3fd5f09db4c38
Ricepaste <n22126@gmail.com>
2024-06-13 17:21:00 +0800

    Update Siamese_Model for binary classification, modified dataloader

--- LabelTool/Scoring_Tool.py
+++ LabelTool/Scoring_Tool.py
@@ -7,6 +7,7 @@ from Elo import Elo
 from Split_Label import Split_Label
 from Preprocessing import Preprocessing
 
+
 class ScoringTool:
     def __init__(self, FOLDER_NAME):
         self.FOLDER_NAME = FOLDER_NAME
@@ -24,18 +25,30 @@ class ScoringTool:
         self.canvas.place(relx=0, rely=0, relwidth=1, relheight=1)
 
         self.score_label = tk.Label(
-            self.window, text="Which one is better?", font=("Arial", 20), bg="white", fg="black")
+            self.window,
+            text="Which one is better?",
+            font=("Arial", 20),
+            bg="white",
+            fg="black",
+        )
         self.score_label.place(x=270, y=40)
 
         self.score_label1 = tk.Label(
-            self.window, text="Click on the Image if you like it !!!", font=("Arial", 15))
+            self.window,
+            text="Click on the Image if you like it !!!",
+            font=("Arial", 15),
+        )
         self.score_label1.place(x=255, y=100)
 
-        self.image1_button = tk.Button(self.window, image=self.image1, command=lambda: self.scoring(1))
+        self.image1_button = tk.Button(
+            self.window, image=self.image1, command=lambda: self.scoring(1)
+        )
         self.image1_button.place(x=90, y=150)
         self.image1_button.lift()
 
-        self.image2_button = tk.Button(self.window, image=self.image2, command=lambda: self.scoring(2))
+        self.image2_button = tk.Button(
+            self.window, image=self.image2, command=lambda: self.scoring(2)
+        )
         self.image2_button.place(x=450, y=150)
         self.image2_button.lift()
 
@@ -66,17 +79,22 @@ class ScoringTool:
         self.image1_index, self.image2_index = self.image_combinations[random_index]
         self.image_combinations.pop(random_index)
 
-        image1 = Image.open(f"./LabelTool/{self.FOLDER_NAME}/" +
-                            self.file_list[self.image1_index])
+        image1 = Image.open(
+            f"./LabelTool/{self.FOLDER_NAME}/" + self.file_list[self.image1_index]
+        )
         image1 = image1.resize((250, 250))
         image1 = ImageTk.PhotoImage(image1)
 
-        image2 = Image.open(f"./LabelTool/{self.FOLDER_NAME}/" +
-                            self.file_list[self.image2_index])
+        image2 = Image.open(
+            f"./LabelTool/{self.FOLDER_NAME}/" + self.file_list[self.image2_index]
+        )
         image2 = image2.resize((250, 250))
         image2 = ImageTk.PhotoImage(image2)
-        
-        print("Picture 1: " + self.file_list[self.image1_index], ", Picture 2: " + self.file_list[self.image2_index])
+
+        print(
+            "Picture 1: " + self.file_list[self.image1_index],
+            ", Picture 2: " + self.file_list[self.image2_index],
+        )
 
         return image1, image2
 
@@ -84,16 +102,23 @@ class ScoringTool:
         if button_pressed == 1:
             print("Image 1 button pressed")
             write = [self.image1_index, self.image2_index]
-            Elo(self.image1_index, self.image2_index, k=32, FOLDER_NAME = self.FOLDER_NAME)
-            
+            Elo(
+                self.image1_index, self.image2_index, k=32, FOLDER_NAME=self.FOLDER_NAME
+            )
+
         elif button_pressed == 2:
             print("Image 2 button pressed")
             write = [self.image2_index, self.image1_index]
-            Elo(self.image2_index, self.image1_index, k=32, FOLDER_NAME = self.FOLDER_NAME)
+            Elo(
+                self.image2_index, self.image1_index, k=32, FOLDER_NAME=self.FOLDER_NAME
+            )
 
-        self.record = pd.concat([self.record, pd.DataFrame([write], columns=["WinnerID", "LoserID"])], ignore_index=True)
+        self.record = pd.concat(
+            [self.record, pd.DataFrame([write], columns=["WinnerID", "LoserID"])],
+            ignore_index=True,
+        )
         self.record.to_csv(f"./LabelTool/{self.FOLDER_NAME}/record.csv", index=False)
-        
+
         new_image1, new_image2 = self.get_image()
 
         if new_image1 is not None and new_image2 is not None:
@@ -105,7 +130,7 @@ class ScoringTool:
             self.score_label.configure(text="No more images to score")
             self.image1_button.configure(state=tk.DISABLED)
             self.image2_button.configure(state=tk.DISABLED)
-            
+
     def set_background_with_opacity(self, image_path):
         image = Image.open(image_path)
         image = image.convert("RGBA")
@@ -116,8 +141,9 @@ class ScoringTool:
         self.canvas.create_image(0, 0, anchor=tk.NW, image=photo)
         self.canvas.image = photo
 
+
 # 重置所有評分紀錄及圖片分數
-RESET = True
+RESET = False
 
 # 資料集存放的資料夾名稱
 FOLDER_NAME = "backup27"
