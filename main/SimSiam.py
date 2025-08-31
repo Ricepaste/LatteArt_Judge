6089d7419335e0ab6e4a4fecc1ec8bfc6fc67acc
sharpaste <n22126@gmail.com>
2025-08-31 17:59:56 +0800

    experiment: change epochs&method

--- main/SimSiam.py
+++ main/SimSiam.py
@@ -6,11 +6,11 @@ import src.training.SimSiam_train as SimSiam_train
 
 # 設置共用的訓練參數
 common_train_params = {
-    "num_epochs": 100,  # 可以設長一點試試
+    "num_epochs": 150,  # 可以設長一點試試
     "batch_size": 128,
     "workers": 0,  # 根據你的設備調整
     "dataset_dir": ".\\LabelTool",  # 你的資料集路徑
-    "rigl_dense_allocation": 0.6,  # RigL 稀疏度 (90% sparse)
+    "rigl_dense_allocation": 0.65,  # RigL 稀疏度 (90% sparse)
     "rigl_delta": 100,
     "rigl_alpha": 0.3,
     "consistency_lambda": 0.1,
@@ -19,20 +19,20 @@ common_train_params = {
 
 # TODO: 刪除無用的dataset_dir參數
 def main():
-    # model_rigl_baseline = SimSiam_train.SimSiam_Model(base_lr=0.03)
-    # print("\n--- Running Baseline RigL on SimSiam ---")
-    # model_rigl_baseline.train(
-    #     **common_train_params,
-    #     rigl_mode="baseline",  # 設置為 baseline
-    #     grad_cache_chunk_size=0,  # 不使用 GradCache
-    # )
-    model_rigl_consistency = SimSiam_train.SimSiam_Model(base_lr=0.03)
-    print("\n--- Running Consistency RigL on SimSiam ---")
-    model_rigl_consistency.train(
+    model_rigl_baseline = SimSiam_train.SimSiam_Model(base_lr=0.03)
+    print("\n--- Running Baseline RigL on SimSiam ---")
+    model_rigl_baseline.train(
         **common_train_params,
-        rigl_mode="consistency",  # 設置為 consistency
+        rigl_mode="baseline",  # 設置為 baseline
         grad_cache_chunk_size=0,  # 不使用 GradCache
     )
+    # model_rigl_consistency = SimSiam_train.SimSiam_Model(base_lr=0.03)
+    # print("\n--- Running Consistency RigL on SimSiam ---")
+    # model_rigl_consistency.train(
+    #     **common_train_params,
+    #     rigl_mode="consistency",  # 設置為 consistency
+    #     grad_cache_chunk_size=0,  # 不使用 GradCache
+    # )
 
     # model_rigl_consistency = SimSiam_train.SimSiam_Model(base_lr=0.03)
     # print("\n--- Running Consistency RigL on SimSiam ---")
